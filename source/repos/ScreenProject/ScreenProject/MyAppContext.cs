d60a9f8600a2b08c43e68907190ce6c0df6f6d85
alaa frakhna <alaa.frakhna@hotmail.com>
2019-08-01 13:02:54 +0300

    MyAppContext

---
+++ source/repos/ScreenProject/ScreenProject/MyAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenProject
+{
+    public class MyAppContext:DbContext
+    {
+
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+        {
+
+        }
+    }
+}
