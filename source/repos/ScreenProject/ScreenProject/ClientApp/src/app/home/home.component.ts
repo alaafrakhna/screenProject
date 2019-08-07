014eb25f65cc30ea104e52c45be9aaef69241f76
alaa frakhna <alaa.frakhna@hotmail.com>
2019-08-07 07:59:34 +0300

    t5

--- source/repos/ScreenProject/ScreenProject/ClientApp/src/app/home/home.component.ts
+++ source/repos/ScreenProject/ScreenProject/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
+
+
+
 export class HomeComponent {
-}
+
+
+  public events: any[];
+
+  public interval = 5000;  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {    this.GetEventt(http, baseUrl);    this.Reload(http, baseUrl);  }  GetEventt(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {    http.get<any[]>(baseUrl + 'api/Event/GetCurrentEvent').subscribe(result => {      this.events = result;    }, error => console.error(error)    );  }  Reload(http: HttpClient, @Inject('BASE_URL') baseUrl: string): void {    setInterval(() => {      this.GetEventt(http, baseUrl);    }, this.interval);  }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ////setInterval(constructor, 1000);
+
+  //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  //  http.get<any[]>(baseUrl + 'api/Event/GetCurrentEvent').subscribe(result => {
+  //    this.events = result;
+  //  }, error => console.error(error));
+  //}
+
+
+
+
+
+  
+
+
+
+
+
+
+
