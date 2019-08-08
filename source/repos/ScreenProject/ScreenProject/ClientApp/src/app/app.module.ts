80eef6d26283377d2942756c3497f60ef7d13553
alaa frakhna <alaa.frakhna@hotmail.com>
2019-08-08 07:57:39 +0300

    t7

--- source/repos/ScreenProject/ScreenProject/ClientApp/src/app/app.module.ts
+++ source/repos/ScreenProject/ScreenProject/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { MaterialModule } from './material';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +25,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    //BrowserAnimationsModule,
+    //MaterialModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
